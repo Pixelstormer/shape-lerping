a89c284a4bf8c0631911306e2ea5672a15667e38
Pixelstorm <thepixelstormer@gmail.com>
2021-10-19 22:26:03 +0100

    Refactor lerp handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use bevy::prelude::*;
+use bevy_prototype_lyon::entity::Path as PathComponent;
 use bevy_prototype_lyon::prelude::*;
 use lerp::Lerp;
 use std::{
     iter::{self, FromIterator},
     ops::{Add, RangeBounds, RangeInclusive, Sub},
 };
-use tess::path::{path::Builder, Event};
+use tess::{
+    geom::euclid::default::Point2D,
+    path::{path::Builder, Event, Path},
+};
 
 enum Direction {
     Increasing,
@@ -116,7 +120,7 @@ fn setup(mut commands: Commands) {
             direction: Direction::Increasing,
         })
         .insert(LerpingShape {
-            target: ShapePath::build_as(&shape),
+            target: ShapePath::build_as(&shape).0,
             lerp_t: 0.1,
             margin_of_error: 1.0,
         });
@@ -141,143 +145,158 @@ fn update_lerp_target<T: RangeBounds<u8> + 'static + Send + Sync>(
             sides: sides.sides as usize,
             feature: shapes::RegularPolygonFeature::Radius(200.0),
             ..Default::default()
-        });
+        })
+        .0;
     }
 }
 
 fn lerp_shape(
     mut lerp_events: EventWriter<LerpFinished>,
-    mut query: Query<(Entity, &mut Path, &LerpingShape)>,
+    mut query: Query<(Entity, &mut PathComponent, &LerpingShape)>,
 ) {
-    for (entity, mut path, target) in query.iter_mut() {
-        let t = target.lerp_t;
-        let margin_of_error = target.margin_of_error;
-        let target = &target.target;
-
-        let count = path.0.iter().count();
-        let target_count = target.0.iter().count();
-
-        let diff = target_count as isize - count as isize;
-        if match diff.signum() {
-            0 => path.lerp_same_sides(target, count, t, margin_of_error),
-            1 => path.lerp_greater_sides(target, count, target_count, t, margin_of_error),
-            -1 => path.lerp_less_sides(target, count, target_count, t, margin_of_error),
-            _ => unreachable!(),
-        } {
+    for (entity, mut from, to) in query.iter_mut() {
+        let (new_path, result) = from.0.lerp(&to.target, to.lerp_t, to.margin_of_error);
+        from.0 = new_path;
+        if let LerpResult::WithinMarginOfError = result {
             lerp_events.send(LerpFinished(entity));
         }
     }
 }
 
-trait Lerpable {
-    fn lerp_same_sides(
-        &mut self,
-        target: &Path,
-        count: usize,
-        t: f32,
-        margin_of_error: f32,
-    ) -> bool;
-    fn lerp_greater_sides(
-        &mut self,
-        target: &Path,
-        self_count: usize,
-        target_count: usize,
-        t: f32,
-        margin_of_error: f32,
-    ) -> bool;
-    fn lerp_less_sides(
-        &mut self,
-        target: &Path,
-        self_count: usize,
-        target_count: usize,
-        t: f32,
-        margin_of_error: f32,
-    ) -> bool;
+pub enum LerpResult {
+    OutsideMarginOfError,
+    WithinMarginOfError,
+}
+
+trait Lerpable<T = Self> {
+    type Output;
+    fn lerp(&self, target: &T, t: f32, margin_of_error: f32) -> (Self::Output, LerpResult);
 }
 
 impl Lerpable for Path {
-    fn lerp_same_sides(
-        &mut self,
-        target: &Path,
-        count: usize,
+    type Output = Self;
+    fn lerp(&self, target: &Self, t: f32, margin_of_error: f32) -> (Self, LerpResult) {
+        let self_count = self.iter().count();
+        let target_count = target.iter().count();
+
+        if self_count == target_count {
+            path_lerping::lerp_same_sides(self, target, t, margin_of_error)
+        } else if self_count > target_count {
+            path_lerping::lerp_less_sides(self, target, t, margin_of_error)
+        } else {
+            // self_count < target_count
+            path_lerping::lerp_greater_sides(self, target, t, margin_of_error)
+        }
+    }
+}
+
+mod path_lerping {
+    use super::*;
+    pub fn lerp_same_sides(
+        from: &Path,
+        to: &Path,
         t: f32,
         margin_of_error: f32,
-    ) -> bool {
-        use tess::{geom::euclid::default::Point2D, path::Path};
-
-        fn check_if_unfinished(
-            result: Point2D<f32>,
-            target: Point2D<f32>,
+    ) -> (Path, LerpResult) {
+        fn check_if_within_margin_of_error(
+            from: Point2D<f32>,
+            to: Point2D<f32>,
             margin_of_error: f32,
-            all_finished: &mut bool,
+            out: &mut LerpResult,
         ) {
-            if ((result.x - target.x).abs() > margin_of_error)
-                || ((result.y - target.y).abs() > margin_of_error)
+            if ((from.x - to.x).abs() > margin_of_error)
+                || ((from.y - to.y).abs() > margin_of_error)
             {
-                *all_finished = false;
+                *out = LerpResult::OutsideMarginOfError;
             }
         }
 
-        let all_finished: &mut bool = &mut true;
+        let count = from.iter().count();
+        assert!(count == to.iter().count());
 
-        let parts = self
-            .0
-            .iter()
-            .zip(target.0.iter())
-            .map(
-                |(self_event, target_event)| match (self_event, target_event) {
-                    (Event::Begin { at: self_at }, Event::Begin { at: target_at }) => {
+        let mut all_within_margin_of_error = LerpResult::WithinMarginOfError;
+
+        let parts =
+            from.iter()
+                .zip(to.iter())
+                .map(|(from_event, to_event)| match (from_event, to_event) {
+                    (Event::Begin { at: from_at }, Event::Begin { at: to_at }) => {
                         let at = Point2D::new(
-                            Lerp::lerp(self_at.x, target_at.x, t),
-                            Lerp::lerp(self_at.y, target_at.y, t),
+                            Lerp::lerp(from_at.x, to_at.x, t),
+                            Lerp::lerp(from_at.y, to_at.y, t),
                         );
-                        check_if_unfinished(at, target_at, margin_of_error, all_finished);
+                        check_if_within_margin_of_error(
+                            at,
+                            to_at,
+                            margin_of_error,
+                            &mut all_within_margin_of_error,
+                        );
                         Event::Begin { at }
                     }
                     (
                         Event::Line {
-                            from: self_from,
-                            to: self_to,
+                            from: from_from,
+                            to: from_to,
                         },
                         Event::Line {
-                            from: target_from,
-                            to: target_to,
+                            from: to_from,
+                            to: to_to,
                         },
                     ) => {
                         let from = Point2D::new(
-                            Lerp::lerp(self_from.x, target_from.x, t),
-                            Lerp::lerp(self_from.y, target_from.y, t),
+                            Lerp::lerp(from_from.x, to_from.x, t),
+                            Lerp::lerp(from_from.y, to_from.y, t),
                         );
                         let to = Point2D::new(
-                            Lerp::lerp(self_to.x, target_to.x, t),
-                            Lerp::lerp(self_to.y, target_to.y, t),
+                            Lerp::lerp(from_to.x, to_to.x, t),
+                            Lerp::lerp(from_to.y, to_to.y, t),
                         );
-                        check_if_unfinished(from, target_from, margin_of_error, all_finished);
-                        check_if_unfinished(to, target_to, margin_of_error, all_finished);
+                        check_if_within_margin_of_error(
+                            from,
+                            to_from,
+                            margin_of_error,
+                            &mut all_within_margin_of_error,
+                        );
+                        check_if_within_margin_of_error(
+                            to,
+                            to_to,
+                            margin_of_error,
+                            &mut all_within_margin_of_error,
+                        );
                         Event::Line { from, to }
                     }
                     (
                         Event::End {
-                            last: self_last,
-                            first: self_first,
+                            last: from_last,
+                            first: from_first,
                             ..
                         },
                         Event::End {
-                            last: target_last,
-                            first: target_first,
+                            last: to_last,
+                            first: to_first,
                             ..
                         },
                     ) => {
                         let last = Point2D::new(
-                            Lerp::lerp(self_last.x, target_last.x, t),
-                            Lerp::lerp(self_last.y, target_last.y, t),
+                            Lerp::lerp(from_last.x, to_last.x, t),
+                            Lerp::lerp(from_last.y, to_last.y, t),
                         );
                         let first = Point2D::new(
-                            Lerp::lerp(self_first.x, target_first.x, t),
-                            Lerp::lerp(self_first.y, target_first.y, t),
+                            Lerp::lerp(from_first.x, to_first.x, t),
+                            Lerp::lerp(from_first.y, to_first.y, t),
                         );
-                        check_if_unfinished(last, target_last, margin_of_error, all_finished);
-                        check_if_unfinished(first, target_first, margin_of_error, all_finished);
+                        check_if_within_margin_of_error(
+                            last,
+                            to_last,
+                            margin_of_error,
+                            &mut all_within_margin_of_error,
+                        );
+                        check_if_within_margin_of_error(
+                            first,
+                            to_first,
+                            margin_of_error,
+                            &mut all_within_margin_of_error,
+                        );
                         Event::End {
                             last,
                             first,
@@ -285,84 +304,81 @@ impl Lerpable for Path {
                         }
                     }
                     _ => unreachable!(),
-                },
-            );
+                });
 
         let mut builder = Builder::with_capacity(count * 2 - 1, count);
         builder.concatenate(&[Path::from_iter(parts).as_slice()]);
-        self.0 = builder.build();
-
-        *all_finished
+        (builder.build(), all_within_margin_of_error)
     }
 
-    fn lerp_greater_sides(
-        &mut self,
-        target: &Path,
-        self_count: usize,
-        target_count: usize,
+    pub fn lerp_greater_sides(
+        from: &Path,
+        to: &Path,
         t: f32,
         margin_of_error: f32,
-    ) -> bool {
-        use tess::path::Path;
-        let insert_index = self_count / 2;
+    ) -> (Path, LerpResult) {
+        let from_count = from.iter().count();
+        let to_count = to.iter().count();
+        assert!(from_count < to_count);
 
-        if let Event::Line { to, .. } = self.0.iter().nth(insert_index).unwrap() {
-            let diff = target_count - self_count;
-            let parts = self
-                .0
+        let insert_index = from_count / 2;
+        if let Event::Line { to: duplicated, .. } = from.iter().nth(insert_index).unwrap() {
+            let diff = to_count - from_count;
+            let parts = from
                 .iter()
                 .take(insert_index)
-                .chain(iter::repeat(Event::Line { from: to, to }).take(diff))
-                .chain(self.0.iter().skip(insert_index));
+                .chain(
+                    iter::repeat(Event::Line {
+                        from: duplicated,
+                        to: duplicated,
+                    })
+                    .take(diff),
+                )
+                .chain(from.iter().skip(insert_index));
 
-            let mut builder = Builder::with_capacity(target_count * 2 - 1, target_count);
+            let mut builder = Builder::with_capacity(to_count * 2 - 1, to_count);
             builder.concatenate(&[Path::from_iter(parts).as_slice()]);
-            self.0 = builder.build();
-
-            self.lerp_same_sides(target, target_count, t, margin_of_error)
+            lerp_same_sides(&builder.build(), to, t, margin_of_error)
         } else {
             unreachable!()
         }
     }
 
-    fn lerp_less_sides(
-        &mut self,
-        target: &Path,
-        self_count: usize,
-        target_count: usize,
+    pub fn lerp_less_sides(
+        from: &Path,
+        to: &Path,
         t: f32,
         margin_of_error: f32,
-    ) -> bool {
-        use tess::path::Path;
+    ) -> (Path, LerpResult) {
+        let from_count = from.iter().count();
+        let to_count = to.iter().count();
+        assert!(from_count > to_count);
 
-        let diff = self_count - target_count;
-
-        let insert_index = target_count / 2;
-        let duplicated = target.0.iter().nth(insert_index).unwrap();
-        let parts = target
-            .0
+        let diff = from_count - to_count;
+        let insert_index = to_count / 2;
+        let duplicated = to.iter().nth(insert_index).unwrap();
+        let parts = to
             .iter()
             .take(insert_index)
             .chain(iter::repeat(duplicated).take(diff))
-            .chain(target.0.iter().skip(insert_index));
+            .chain(to.iter().skip(insert_index));
 
-        let mut builder = Builder::with_capacity(self_count * 2 - 1, self_count);
+        let mut builder = Builder::with_capacity(from_count * 2 - 1, from_count);
         builder.concatenate(&[Path::from_iter(parts).as_slice()]);
-        let is_finished =
-            self.lerp_same_sides(&Path(builder.build()), self_count, t, margin_of_error);
-        if is_finished {
-            let remove_index = self_count / 2;
 
-            let parts = self
-                .0
+        let (mut lerped, result) = lerp_same_sides(from, &builder.build(), t, margin_of_error);
+        if let LerpResult::WithinMarginOfError = result {
+            let remove_index = from_count / 2;
+
+            let parts = lerped
                 .iter()
                 .take(remove_index)
-                .chain(self.0.iter().skip(remove_index + diff));
+                .chain(lerped.iter().skip(remove_index + diff));
 
-            let mut builder = Builder::with_capacity(target_count * 2 - 1, target_count);
+            let mut builder = Builder::with_capacity(to_count * 2 - 1, to_count);
             builder.concatenate(&[Path::from_iter(parts).as_slice()]);
-            self.0 = builder.build();
+            lerped = builder.build();
         }
-        is_finished
+        (lerped, result)
     }
 }
