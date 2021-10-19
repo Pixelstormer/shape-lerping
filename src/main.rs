7a68edcfd2326a4df3eddd6905df00ab87ea82f0
Pixelstorm <thepixelstormer@gmail.com>
2021-10-19 21:00:14 +0100

    Use struct with named fields over tuple structs for clarity and refactor side range handling

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,67 @@
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 use lerp::Lerp;
-use std::iter::{self, FromIterator};
+use std::{
+    iter::{self, FromIterator},
+    ops::{Add, RangeBounds, RangeInclusive, Sub},
+};
 use tess::path::{path::Builder, Event};
 
-#[derive(Component)]
-struct SidesChangingShape(isize);
+enum Direction {
+    Increasing,
+    Decreasing,
+}
+
+impl Direction {
+    fn inverted(&self) -> Self {
+        match self {
+            Direction::Increasing => Direction::Decreasing,
+            Direction::Decreasing => Direction::Increasing,
+        }
+    }
+
+    fn invert(&mut self) {
+        *self = self.inverted();
+    }
+
+    fn get_operation<T: Add<Rhs, Output = Output> + Sub<Rhs, Output = Output>, Rhs, Output>(
+        &self,
+    ) -> &dyn Fn(T, Rhs) -> Output {
+        match self {
+            Direction::Increasing => &Add::add,
+            Direction::Decreasing => &Sub::sub,
+        }
+    }
+}
 
 #[derive(Component)]
-struct SidesRange(isize, isize);
+struct SidesChangingShape<T: RangeBounds<u8>> {
+    sides: u8,
+    bounds: T,
+    direction: Direction,
+}
+
+impl<T: RangeBounds<u8>> SidesChangingShape<T> {
+    fn increment_sides(&mut self) {
+        let op = self.direction.get_operation();
+        let new_sides = op(self.sides, 1);
+        if self.bounds.contains(&new_sides) {
+            self.sides = new_sides;
+        } else {
+            self.direction.invert();
+            self.increment_sides();
+        }
+    }
+}
 
 #[derive(Component)]
-struct LerpingShape(Path, f32, f32);
+struct LerpingShape {
+    target: Path,
+    lerp_t: f32,
+    margin_of_error: f32,
+}
 
+// Event for when all points of a LerpingShape are within the margin-of-error of the target path
 struct LerpFinished(Entity);
 
 fn main() {
@@ -22,9 +71,13 @@ fn main() {
         .add_plugin(ShapePlugin)
         .add_startup_system(setup.system())
         .add_event::<LerpFinished>()
-        .add_system(change_sides.system().label("change_sides"))
         .add_system(
-            update_lerp_target
+            change_sides::<RangeInclusive<u8>>
+                .system()
+                .label("change_sides"),
+        )
+        .add_system(
+            update_lerp_target::<RangeInclusive<u8>>
                 .system()
                 .label("update_lerp_target")
                 .after("change_sides"),
@@ -57,37 +110,35 @@ fn setup(mut commands: Commands) {
             },
             Transform::default(),
         ))
-        .insert(SidesChangingShape(SIDES as isize))
-        .insert(SidesRange(3, 8))
-        .insert(LerpingShape(ShapePath::build_as(&shape), 0.1, 1.0));
+        .insert(SidesChangingShape {
+            sides: SIDES,
+            bounds: 3..=8,
+            direction: Direction::Increasing,
+        })
+        .insert(LerpingShape {
+            target: ShapePath::build_as(&shape),
+            lerp_t: 0.1,
+            margin_of_error: 1.0,
+        });
 }
 
-fn change_sides(
+fn change_sides<T: RangeBounds<u8> + 'static + Send + Sync>(
     mut lerp_events: EventReader<LerpFinished>,
-    mut query: Query<(&mut SidesChangingShape, &SidesRange)>,
+    mut query: Query<&mut SidesChangingShape<T>>,
 ) {
     for LerpFinished(entity) in lerp_events.iter() {
-        if let Ok((mut sides, SidesRange(min, max))) = query.get_mut(*entity) {
-            let (min, max) = (*min, *max);
-            if sides.0 == max {
-                sides.0 = -(max - 1);
-            } else if sides.0 == -min {
-                sides.0 = min + 1;
-            } else if (sides.0 >= min && sides.0 < max) || (sides.0 >= -max && sides.0 < -min) {
-                sides.0 += 1;
-            } else {
-                sides.0 = min;
-            }
+        if let Ok(mut sides) = query.get_mut(*entity) {
+            sides.increment_sides();
         }
     }
 }
 
-fn update_lerp_target(
-    mut query: Query<(&SidesChangingShape, &mut LerpingShape), Changed<SidesChangingShape>>,
+fn update_lerp_target<T: RangeBounds<u8> + 'static + Send + Sync>(
+    mut query: Query<(&SidesChangingShape<T>, &mut LerpingShape), Changed<SidesChangingShape<T>>>,
 ) {
     for (sides, mut shape) in query.iter_mut() {
-        shape.0 = ShapePath::build_as(&shapes::RegularPolygon {
-            sides: sides.0.abs() as usize,
+        shape.target = ShapePath::build_as(&shapes::RegularPolygon {
+            sides: sides.sides as usize,
             feature: shapes::RegularPolygonFeature::Radius(200.0),
             ..Default::default()
         });
@@ -99,18 +150,18 @@ fn lerp_shape(
     mut query: Query<(Entity, &mut Path, &LerpingShape)>,
 ) {
     for (entity, mut path, target) in query.iter_mut() {
-        let t = target.1;
-        let precision = target.2;
-        let target = &target.0;
+        let t = target.lerp_t;
+        let margin_of_error = target.margin_of_error;
+        let target = &target.target;
 
         let count = path.0.iter().count();
         let target_count = target.0.iter().count();
 
         let diff = target_count as isize - count as isize;
         if match diff.signum() {
-            0 => path.lerp_same_sides(target, count, t, precision),
-            1 => path.lerp_greater_sides(target, count, target_count, t, precision),
-            -1 => path.lerp_less_sides(target, count, target_count, t, precision),
+            0 => path.lerp_same_sides(target, count, t, margin_of_error),
+            1 => path.lerp_greater_sides(target, count, target_count, t, margin_of_error),
+            -1 => path.lerp_less_sides(target, count, target_count, t, margin_of_error),
             _ => unreachable!(),
         } {
             lerp_events.send(LerpFinished(entity));
@@ -119,14 +170,20 @@ fn lerp_shape(
 }
 
 trait Lerpable {
-    fn lerp_same_sides(&mut self, target: &Path, count: usize, t: f32, precision: f32) -> bool;
+    fn lerp_same_sides(
+        &mut self,
+        target: &Path,
+        count: usize,
+        t: f32,
+        margin_of_error: f32,
+    ) -> bool;
     fn lerp_greater_sides(
         &mut self,
         target: &Path,
         self_count: usize,
         target_count: usize,
         t: f32,
-        precision: f32,
+        margin_of_error: f32,
     ) -> bool;
     fn lerp_less_sides(
         &mut self,
@@ -134,22 +191,28 @@ trait Lerpable {
         self_count: usize,
         target_count: usize,
         t: f32,
-        precision: f32,
+        margin_of_error: f32,
     ) -> bool;
 }
 
 impl Lerpable for Path {
-    fn lerp_same_sides(&mut self, target: &Path, count: usize, t: f32, precision: f32) -> bool {
+    fn lerp_same_sides(
+        &mut self,
+        target: &Path,
+        count: usize,
+        t: f32,
+        margin_of_error: f32,
+    ) -> bool {
         use tess::{geom::euclid::default::Point2D, path::Path};
 
         fn check_if_unfinished(
             result: Point2D<f32>,
             target: Point2D<f32>,
-            precision: f32,
+            margin_of_error: f32,
             all_finished: &mut bool,
         ) {
-            if ((result.x - target.x).abs() > precision)
-                || ((result.y - target.y).abs() > precision)
+            if ((result.x - target.x).abs() > margin_of_error)
+                || ((result.y - target.y).abs() > margin_of_error)
             {
                 *all_finished = false;
             }
@@ -168,7 +231,7 @@ impl Lerpable for Path {
                             Lerp::lerp(self_at.x, target_at.x, t),
                             Lerp::lerp(self_at.y, target_at.y, t),
                         );
-                        check_if_unfinished(at, target_at, precision, all_finished);
+                        check_if_unfinished(at, target_at, margin_of_error, all_finished);
                         Event::Begin { at }
                     }
                     (
@@ -189,8 +252,8 @@ impl Lerpable for Path {
                             Lerp::lerp(self_to.x, target_to.x, t),
                             Lerp::lerp(self_to.y, target_to.y, t),
                         );
-                        check_if_unfinished(from, target_from, precision, all_finished);
-                        check_if_unfinished(to, target_to, precision, all_finished);
+                        check_if_unfinished(from, target_from, margin_of_error, all_finished);
+                        check_if_unfinished(to, target_to, margin_of_error, all_finished);
                         Event::Line { from, to }
                     }
                     (
@@ -213,8 +276,8 @@ impl Lerpable for Path {
                             Lerp::lerp(self_first.x, target_first.x, t),
                             Lerp::lerp(self_first.y, target_first.y, t),
                         );
-                        check_if_unfinished(last, target_last, precision, all_finished);
-                        check_if_unfinished(first, target_first, precision, all_finished);
+                        check_if_unfinished(last, target_last, margin_of_error, all_finished);
+                        check_if_unfinished(first, target_first, margin_of_error, all_finished);
                         Event::End {
                             last,
                             first,
@@ -238,7 +301,7 @@ impl Lerpable for Path {
         self_count: usize,
         target_count: usize,
         t: f32,
-        precision: f32,
+        margin_of_error: f32,
     ) -> bool {
         use tess::path::Path;
         let insert_index = self_count / 2;
@@ -256,7 +319,7 @@ impl Lerpable for Path {
             builder.concatenate(&[Path::from_iter(parts).as_slice()]);
             self.0 = builder.build();
 
-            self.lerp_same_sides(target, target_count, t, precision)
+            self.lerp_same_sides(target, target_count, t, margin_of_error)
         } else {
             unreachable!()
         }
@@ -268,7 +331,7 @@ impl Lerpable for Path {
         self_count: usize,
         target_count: usize,
         t: f32,
-        precision: f32,
+        margin_of_error: f32,
     ) -> bool {
         use tess::path::Path;
 
@@ -285,7 +348,8 @@ impl Lerpable for Path {
 
         let mut builder = Builder::with_capacity(self_count * 2 - 1, self_count);
         builder.concatenate(&[Path::from_iter(parts).as_slice()]);
-        let is_finished = self.lerp_same_sides(&Path(builder.build()), self_count, t, precision);
+        let is_finished =
+            self.lerp_same_sides(&Path(builder.build()), self_count, t, margin_of_error);
         if is_finished {
             let remove_index = self_count / 2;
 
